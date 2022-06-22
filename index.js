41f60f9dcaff681f732729120a7012c8c7267742
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-22 14:24:52 +0200

    array b v2 exercises

---
+++ index.js
@@ -0,0 +1,49 @@
+// exercise 1
+
+// let evenSum = 0
+
+// for (let i = 11; i < 47; i++){
+//   if(i % 2 == 0){
+//     evenSum += i
+//   }
+// }
+
+// console.log(evenSum)
+
+// exercise 2
+
+// let oddSum = 0
+
+// for (let i = 109; i < 588; i++){
+//   if(i % 2 == 0){
+//     continue
+//   }else{
+//     console.log(i)
+//     oddSum += i
+//   }
+// }
+
+// console.log(oddSum)
+
+// exercise 3
+
+// const numbers = [1, 2, 3, 4, 5]
+// const squares = []
+// const roots = []
+
+// for(let i = 0; i < numbers.length; i++){
+//   squares.push(numbers[i]**2)
+// }
+// console.log(squares)
+
+
+// for(let j = 0; j < squares.length; j++){
+//   roots.push(Math.sqrt(squares[j]))
+// }
+// console.log(roots)
+
+// exercise 4
+
+// for (let i = 0; i <= 10; i++){
+//   console.log(`3 x ${i} = ${3*i}`)
+// }
